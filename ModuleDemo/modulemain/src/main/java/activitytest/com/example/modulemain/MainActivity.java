2225b5faefed4845d2b9bd4568e8d6f52c5d350f
liyao <904117257@qq.com>
2021-07-03 17:04:35 +0800

    commit

---
+++ ModuleDemo/modulemain/src/main/java/activitytest/com/example/modulemain/MainActivity.java
@@ -0,0 +1,14 @@
+package activitytest.com.example.modulemain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

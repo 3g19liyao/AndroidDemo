7f227213955d8131f5225239869bf901d968be8e
liyao <904117257@qq.com>
2021-11-01 16:47:51 +0800

    commit

---
+++ OkHttp/app/src/main/java/activitytest/com/example/okhttp/MainActivity.java
@@ -0,0 +1,57 @@
+package activitytest.com.example.okhttp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView responseText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button sendRequest = (Button)findViewById(R.id.send_request);
+        responseText = (TextView)findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.send_request){
+            sendRequestWithOkHttp();
+        }
+    }
+
+    private void sendRequestWithOkHttp() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+            }
+        }).start();
+    }
+    private void showResponse(final String response){
+        runOnUiThread(new Runnable() {                          //安卓不允许子线程UI操作，这个会切换到主线程
+            @Override
+            public void run() {
+                responseText.setText(response);
+            }
+        });
+    }
+}
